7b547a622e6bfcb5d43277fc223f690c67012941
Mounir El Bakkali <mounir.elbakkali1@etu.uae.ac.ma>
2024-01-13 13:37:56 +0100

    added condidate suggestion interface 001

---
+++ src/components/SuggestCandidates.tsx
@@ -0,0 +1,16 @@
+
+
+
+const SuggestCandidates = () => {
+
+
+    return (
+        <div>
+            <h1>Suggest Candidates</h1>
+        </div>
+    );
+
+
+}
+
+export default SuggestCandidates;
