9b3fd3f525f0e2e52da673ef8c200bb8a2163997
Mounir El Bakkali <mounir.elbakkali1@etu.uae.ac.ma>
2024-01-13 17:11:24 +0100

    update: nav style

--- src/components/shared/NavBar.tsx
+++ src/components/shared/NavBar.tsx
@@ -9,7 +9,6 @@ const NavBar = () => {
     return (
         <div className="navbar">
             <div className="navbar__logo">
-                <img src="/logo.png" alt="logo" />
             </div>
             <div className="navbar__links">
                 <div>
