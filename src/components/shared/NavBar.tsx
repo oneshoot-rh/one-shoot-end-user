1d5ea49183b7693beb7172fd5f7effca5b04ee69
Mounir El Bakkali <mounir.elbakkali1@etu.uae.ac.ma>
2024-01-13 13:36:38 +0100

    added shared navbar component

---
+++ src/components/shared/NavBar.tsx
@@ -0,0 +1,28 @@
+import { Link } from "react-router-dom";
+import "../styles/NavBar.css"
+
+
+
+const NavBar = () => {
+
+
+    return (
+        <div className="navbar">
+            <div className="navbar__logo">
+                <img src="/logo.png" alt="logo" />
+            </div>
+            <div className="navbar__links">
+                <div>
+                    <Link to="/">Home</Link>
+                    <Link to="/Process">Process</Link>
+                </div>
+                <div>
+                    <Link to="/contact">Login</Link>
+                </div>
+            </div>
+        </div>
+    );
+}
+    
+
+export default NavBar
