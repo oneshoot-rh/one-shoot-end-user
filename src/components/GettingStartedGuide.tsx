3ef6de105325b2b8996770fa2a9c96c4a7987666
Mounir El Bakkali <mounir.elbakkali1@etu.uae.ac.ma>
2024-01-30 14:47:28 +0100

    new: getting started page

---
+++ src/components/GettingStartedGuide.tsx
@@ -0,0 +1,77 @@
+import { Box, Button } from "@mui/material"
+import SensorOccupiedIcon from '@mui/icons-material/SensorOccupied';
+import Diversity3Icon from '@mui/icons-material/Diversity3';
+import FlipClockCountdown from "@leenguyen/react-flip-clock-countdown";
+import '@leenguyen/react-flip-clock-countdown/dist/index.css';
+import "./styles/GettingStarted.css"
+import GetAppIcon from '@mui/icons-material/GetApp';
+
+
+const GettingStartedGuide = () => {
+
+
+    const endDate = new Date("2024-03-10T00:00:00.000Z");
+
+    return (
+        <>
+            <Box sx={{ display: 'flex', padding: "2rem" ,paddingTop:"3rem",fontSize:"1.3rem" }}>
+                <Box>
+                    <h1 className="get__started__title">Enhanced Technologies to Accelerate Your Recruitment Process</h1>
+                    <Box sx={{ display: 'flex' ,gap:"20px" }}>
+                        <span className="features_span">
+                            <span>
+                                <SensorOccupiedIcon />
+                            </span>
+                             1:1 Resume Analysis and Recommendation
+                        </span>
+                        <span className="features_span">
+                            <span>
+                                <Diversity3Icon />
+                            </span>
+                            1:2 E-Onboarding
+                        </span>
+                    </Box>
+                    <p className="main-title">Digitalizing the entire recruitment process</p>
+                    <Box>
+                        <span>Free Demo Ends In: </span>
+                        {/* counterdown  */}
+                        <div className="counterdown__container">
+                            <FlipClockCountdown
+                            to={endDate}
+                            labels={['DAYS', 'HOURS', 'MINUTES', 'SECONDS']}
+                            labelStyle={{ fontSize: 10, fontWeight: 500, textTransform: 'uppercase' }}
+                            digitBlockStyle={{ width: 40, height: 60, fontSize: 30 }}
+                            dividerStyle={{ color: 'white', height: 1 }}
+                            separatorStyle={{ color: 'red', size: '6px' }}
+                            duration={0.5}
+                            className='flip-clock' 
+                        >
+                            Finished
+                        </FlipClockCountdown>
+                        </div>
+                    </Box>
+                    <Box sx={{ display: "flex" ,gap:"20px",marginTop:"20px" }}>
+                        <button  className="btn__call_to_action">
+                            Request Demo
+                        </button>
+                        <button  className="btn__call_to_action__outlined" >
+                           <GetAppIcon/> Download Guide
+                        </button>
+                    </Box>
+                    <small className="small__text">* Free Demo is available for a limited time only</small>
+                </Box>
+                <Box>
+                    <div className="box_digital_image">
+                        <img src="/images/digital.png" alt="digital" />
+                    </div>
+                </Box>
+            </Box>
+        </>    
+    )
+    
+
+}
+
+
+
+export default GettingStartedGuide
